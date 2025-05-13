7926871eb8eb768fb912abd93de0033b72445d61
Fabian Klingenberg <klingenberg.fabian@gmx.de>
2025-05-13 17:09:39 +0200

    jsx version

--- src/entry-server.tsx
+++ src/entry-server.tsx
@@ -1,8 +1,8 @@
 import library from "hydro-js/server";
-const { render: renderToDOM, $, renderToString } = await library;
+const { render: renderToDOM, renderToString } = await library;
 
 const App = (await import("./App")).default; // Vite production buils fails, if it is not imported dynamically
-renderToDOM(App(), $("#app")!, false);
+renderToDOM(<App />, "#app", false);
 
 export async function render(_url: string) {
   const html = renderToString();
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,50 @@
 import { defineConfig } from "vite";
 
+const JSX_TOKEN = "/*Add JSX*/";
+const JSX_TOKEN_SEMICOLON = `${JSX_TOKEN};`;
+
 // https://vite.dev/config/
 export default defineConfig({
   build: {
     target: "esnext",
   },
+  esbuild: {
+    jsxFactory: "h",
+    jsxFragment: "h",
+    jsxInject: JSX_TOKEN,
+  },
+  plugins: [JSX_PLUGIN()],
 });
+
+function JSX_PLUGIN() {
+  return {
+    name: "my-ssr",
+    transform(code: string, _id: string, options?: { ssr?: boolean }) {
+      if (code.startsWith(JSX_TOKEN_SEMICOLON)) {
+        if (options?.ssr) {
+          const hImport = "\nconst { h } = await library;\n";
+
+          if (code.includes("hydro-js/server")) {
+            code = code.replace(JSX_TOKEN_SEMICOLON, "");
+            code = code.replace(
+              /from ["']hydro-js\/server["']/,
+              `from "hydro-js/server";${hImport}`
+            );
+          } else {
+            code = code.replace(
+              JSX_TOKEN_SEMICOLON,
+              `import library from "hydro-js/server";${hImport}`
+            );
+          }
+        } else {
+          code = code.replace(
+            JSX_TOKEN_SEMICOLON,
+            'import { h } from "hydro-js";\n'
+          );
+        }
+
+        return code;
+      }
+    },
+  };
+}
